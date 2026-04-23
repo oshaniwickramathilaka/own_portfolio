9a927be2e98b38e69d0c319970369556fc91809b
Oshani Wickramathilaka <rashiprebhaoshani@gmail.com>
2026-04-23 20:23:26 +0530

    create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer=()=>{
+    return(
+        <div className='py-8 bg-dark-300'>
+            <div className='container mx-auto px-6 text-center'>
+                <p className='text-white'>@ 2026 Created By Oshani Wickramathilaka.All rights reserved.</p>
+
+            </div>
+        </div>
+    )
+}
+export default Footer
